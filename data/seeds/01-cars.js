e15ef20952e801184a1176a765fb6a6e97eaaed8
Chase Darnall <emailchased@gmail.com>
2024-07-04 00:26:42 -0500

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [{
+    vin: '1111111111211',
+    make: 'toyota',
+    model: 'prius',
+    mileage: 123000,
+    title: 'clean',
+    transmission: 'manual'
+}, 
+{
+    vin: '1111113111111',
+    make: 'toyota',
+    model: 'corolla',
+    mileage: 111000,
+    title: 'salvage',
+    
+},
+{
+    vin: '1111121111111',
+    make: 'ford',
+    model: 'focus',
+    mileage: 10000,
+}]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+    
+}
